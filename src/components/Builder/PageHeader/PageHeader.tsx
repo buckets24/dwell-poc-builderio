3a0fb543754b936cabfe723a210a47a702ad207b
CarloCarpio <carlo@ohmyhome.com>
2024-08-12 16:51:44 +0800

    added page border

--- src/components/Builder/PageHeader/PageHeader.tsx
+++ src/components/Builder/PageHeader/PageHeader.tsx
@@ -12,7 +12,7 @@ const PageHeader: React.FC<Props> = ({
 }) => {
   return (
     <div className="relative w-full flex flex-row items-center gap-8">
-      <div className="flex-1 max-w-[120px]">
+      <div className="flex-1 max-w-[104px] max-h-[72px]">
         <img src={logo} alt="Dwell - Student Living" />
         <h2 className="sr-only">Dwell - Student Living</h2>
       </div>
